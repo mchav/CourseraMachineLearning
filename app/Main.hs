1b6adaace34442ade9cfc69331a09669d2635c8f
Michael Chavinda <mchavinda@google.com>
2018-08-14 21:34:48 -0700

    End of Part 3

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,86 @@
 module Main where
 
 import Control.Monad
-import Graphics.Gnuplot.Simple
-import LinearRegression (computeCost, gradientDescent)
+import qualified Graphics.Gnuplot.Advanced as GP
+
+import qualified Graphics.Gnuplot.Graph as Graph
+
+import qualified Graphics.Gnuplot.Plot.ThreeDimensional as Plot3D
+import qualified Graphics.Gnuplot.Graph.ThreeDimensional as Graph3D
+
+import qualified Graphics.Gnuplot.Plot.TwoDimensional as Plot2D
+import qualified Graphics.Gnuplot.Graph.TwoDimensional as Graph2D
+import qualified Graphics.Gnuplot.Terminal.Default as DefaultTerm
+import Graphics.Gnuplot.Plot.TwoDimensional (linearScale, )
+
+import LinearRegression
 import Numeric.LinearAlgebra
+import Prelude hiding ((<>))
+import System.Directory
 import Util (plot, zeros, ones)
 
 -- replace this with the path to your data.
-exerciseOneDataOne :: String
-exerciseOneDataOne = "<app-directory>/ex1data1.txt"
+exerciseOneDataOne :: IO String
+exerciseOneDataOne = do
+  home <- getHomeDirectory
+  return (home ++ "/code/haskell/CourseraMachineLearning/app/ex1data1.txt")
 
-a :: Matrix Double
-a = ident 5
-
-xLabel = XLabel "Population of the City in 10'000s"
-yLabel = YLabel "Profit in $10'000s"
+plotPoints :: [Double] -> [Double] -> Plot2D.T Double Double
+plotPoints x y = Plot2D.function Graph2D.points x lookupValue
+  where lookupValue v = maybe 0 id (lookup v (zip x y))
+  
+plotLine :: [Double] -> [Double] -> Plot2D.T Double Double
+plotLine x y = Plot2D.function Graph2D.lines x lookupValue
+  where lookupValue v = maybe 0 id (lookup v (zip x y))
 
 exerciseOne = do
   -- Part 1: Basic function
   putStrLn "Running warmUpExercise ..."
   putStrLn "5x5 Identity Matrix: "
+  let a = ident 5 :: Matrix Double
   print a
+
   -- Part 2: Plotting
   putStrLn "Plotting Data ..."
-  [x', y] <- liftM toColumns (loadMatrix exerciseOneDataOne)
-  plot [xLabel, yLabel] x' y
+  dataOne <- exerciseOneDataOne
+  [x', y'] <- liftM toColumns (loadMatrix dataOne)
+  GP.plotSync DefaultTerm.cons (plotPoints (toList x') (toList y'))
+
   -- Part 3: Cost and Gradient Descent
-  let m = size y
+  let m = size y'
   let x = (ones m 1) ||| (reshape 1 x')
+  let y = asColumn y'
+  let xPoints = toList x'
+  let yPoints = toList y'
   let theta = zeros 2 1
   let iterations = 1500
   let alpha = 0.01
+  let alpha' = 0.001
   putStrLn "Testing the cost function ..."
-  print $ computeCost x y theta
-  let theta' = (asColumn $ 2 |> [-1, 2])
-  print $ computeCost x y theta'
-  print $ gradientDescent x y theta alpha iterations
+  let j = computeCost x y theta
+  putStrLn ("With theta = [0, 0]\n Cost computed = " ++ (show j))
+  putStrLn "Expected cost value (approx) 32.07"
+  let k = computeCost x y (asColumn $ 2 |> [-1, 2])
+  putStrLn ("With theta = [-1, 2]\n Cost computed = " ++ (show k))
+  putStrLn "Expected cost value (approx) 54.24"
+  
+  putStrLn "Running Gradient Descent ..."
+  let thetaOpt = gradientDescent  x y theta alpha iterations
+  putStrLn ("Theta found by gradient descent: " ++ (show thetaOpt))
+  putStrLn "Expected theta values (approx)\n"
+  putStrLn " -3.6303\n  1.1664"
+  let yOptPoints = (toList (flatten $ x <> thetaOpt))
+  GP.plotSync DefaultTerm.cons $ (plotLine xPoints yOptPoints) `mappend` (plotPoints xPoints yPoints)
+  
+  -- Predicting values
+  let predict1 = row [1, 3.5] <> thetaOpt
+  putStrLn "For population = 35,000 we predict a profit of "
+  print ((predict1 * 1000) `atIndex` (0,0))
+  
+  let predict2 = row [1, 20] <> thetaOpt
+  putStrLn "For population = 70,000 we predict a profit of "
+  print ((predict2 * 1000) `atIndex` (0,0))
+
   
 main = do
   exerciseOne
--- src/LinearRegression.hs
+++ src/LinearRegression.hs
@@ -1,17 +1,48 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
 module LinearRegression where
 
 import Numeric.LinearAlgebra
 import Prelude hiding ((<>))
 
-computeCost :: Matrix Double -> Vector Double -> Matrix Double -> Double
+computeCost :: Matrix Double -- Features
+            -> Matrix Double -- Labels
+            -> Matrix Double -- Theta
+            -> Double
 computeCost x y theta = (sumElements squareDifference) / (2.0 * m)
-  where m = fromIntegral (size y)
+  where m = fromIntegral ((fst . size) y)
         h = x <> theta
-        squareDifference = (h - (asColumn y)) ^ 2
+        squareDifference = (h - y) ^ 2
 
-gradientDescent :: Matrix Double -> Vector Double -> Matrix Double -> Double -> Int -> Matrix Double
+-- A simple gradient descent algorithm that specifies an arbitrary number of iterations.
+gradientDescent :: Matrix Double -- Features
+                -> Matrix Double -- Lables
+                -> Matrix Double -- Parameters
+                -> Double        -- Learning Rate
+                -> Int           -- Number of iterations
+                -> Matrix Double
 gradientDescent _ _ theta _     0        = theta
 gradientDescent x y theta alpha numIters = gradientDescent x y theta' alpha (numIters - 1)
   where theta' = theta - step
-        step = (scalar alpha) * ((tr' x) <> ((x <> theta) - (asColumn y))) / (scalar m) :: Matrix Double
-        m = fromIntegral (size y)
+        step = (scalar alpha) * ((tr' x) <> ((x <> theta) - y)) / (scalar m)
+        m = fromIntegral ((fst . size) y)
+
+gradientDescent' :: Matrix Double -- Features
+                 -> Matrix Double -- labels
+                 -> Matrix Double -- theta
+                 -> Double        -- alpha
+                 -> Double        -- tolerance
+                 -> [Double]      -- cost history
+                 -> ([Double], Matrix Double)
+gradientDescent' x y theta alpha tolerance hist
+  | cost > tolerance = gradientDescent' x y theta' alpha tolerance hist'
+  | otherwise        = (hist' , theta)
+    where cost  = computeCost x y theta
+          hist' = cost : hist
+          theta' = theta - step
+          step = (scalar alpha) * ((tr' x) <> ((x <> theta) - y)) / (scalar m)
+          m = fromIntegral ((fst . size) y)
+
+
+
+
